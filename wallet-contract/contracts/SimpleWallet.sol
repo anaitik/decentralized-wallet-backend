db7eb0cd2e0654f5fb5d8a157d4836df329cdd4d
anaitik <nitikverma72@gmail.com>
2023-12-03 08:06:10 +0530

    wallet contract containing simple functions to send,receive and show

---
+++ wallet-contract/contracts/SimpleWallet.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract SimpleWallet {
+    mapping(address => uint256) public balances;
+
+    event Deposit(address indexed depositor, uint256 amount);
+    event Withdrawal(address indexed beneficiary, uint256 amount);
+
+    function deposit() external payable {
+        require(msg.value > 0, "Deposit amount must be greater than 0");
+
+        balances[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw(uint256 _amount) external {
+        require(_amount > 0, "Withdrawal amount must be greater than 0");
+        require(_amount <= balances[msg.sender], "Insufficient balance");
+
+        balances[msg.sender] -= _amount;
+        payable(msg.sender).transfer(_amount);
+        emit Withdrawal(msg.sender, _amount);
+    }
+
+    function getBalance() external view returns (uint256) {
+        return balances[msg.sender];
+    }
+}
---
+++ wallet-contract/hardhat.config.js
@@ -0,0 +1,102 @@
+require("@matterlabs/hardhat-zksync-solc");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  zksolc: {
+    version: "1.3.9",
+    compilerSource: "binary",
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+    },
+  },
+  networks: {
+    zksync_testnet: {
+      url: "https://zksync2-testnet.zksync.dev",
+      ethNetwork: "goerli",
+      chainId: 280,
+      zksync: true,
+    },
+    zksync_mainnet: {
+      url: "https://zksync2-mainnet.zksync.io/",
+      ethNetwork: "mainnet",
+      chainId: 324,
+      zksync: true,
+    },
+  },
+  paths: {
+    artifacts: "./artifacts-zk",
+    cache: "./cache-zk",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  solidity: {
+    version: "0.8.17",
+    solidity: {
+      version: '0.8.9',
+      defaultNetwork: 'goerli',
+      networks: {
+        hardhat: {},
+        goerli: {
+          url: 'https://rpc.ankr.com/eth_goerli',
+          accounts: [`0x${process.env.PRIVATE_KEY}`]
+        }
+      },
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
+    },
+  },
+};
+
+
+
+// require("@matterlabs/hardhat-zksync-solc");
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   zksolc: {
+//     version: "1.3.9",
+//     compilerSource: "binary",
+//     settings: {
+//       optimizer: {
+//         enabled: true,
+//       },
+//     },
+//   },
+//   networks: {
+//     zksync_testnet: {
+//       url: "https://rinkeby-api.zksync.dev",
+//       chainId: 80001, // Rinkeby chain ID
+//       zksync: true,
+//     },
+//     // Add other networks if needed
+//   },
+//   paths: {
+//     artifacts: "./artifacts-zk",
+//     cache: "./cache-zk",
+//     sources: "./contracts",
+//     tests: "./test",
+//   },
+//   solidity: {
+//     version: "0.8.17",
+//     defaultNetwork: "zksync_testnet",
+//     networks: {
+//       hardhat: {},
+//       zksync_testnet: {
+//         url: "https://rinkeby-api.zksync.dev",
+//         accounts: [`0x${process.env.PRIVATE_KEY}`],
+//       },
+//     },
+//     settings: {
+//       optimizer: {
+//         enabled: true,
+//         runs: 200,
+//       },
+//     },
+//   },
+// };
