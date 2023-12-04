ac894af7736f95d8880a7c70f040a42be95babd3
anaitik <nitikverma72@gmail.com>
2023-12-04 05:45:34 +0530

    adding function for account creation

--- wallet-contract/contracts/SimpleWallet.sol
+++ wallet-contract/contracts/SimpleWallet.sol
@@ -3,12 +3,23 @@ pragma solidity ^0.8.9;
 
 contract SimpleWallet {
     mapping(address => uint256) public balances;
+    mapping(address => bool) public hasAccount;
 
     event Deposit(address indexed depositor, uint256 amount);
     event Withdrawal(address indexed beneficiary, uint256 amount);
     event Transfer(address indexed sender, address indexed recipient, uint256 amount);
 
+    function createAccount() external {
+        require(!hasAccount[msg.sender], "Account already exists");
+        
+        // You can use a more sophisticated method to generate a unique address
+        // For simplicity, we'll use the sender's address as the unique identifier
+        hasAccount[msg.sender] = true;
+        emit Transfer(address(0), msg.sender, 0); // This can be used as a signal for the account creation
+    }
+
     function deposit() external payable {
+        require(hasAccount[msg.sender], "Account does not exist");
         require(msg.value > 0, "Deposit amount must be greater than 0");
 
         balances[msg.sender] += msg.value;
@@ -16,6 +27,7 @@ contract SimpleWallet {
     }
 
     function withdraw(uint256 _amount) external {
+        require(hasAccount[msg.sender], "Account does not exist");
         require(_amount > 0, "Withdrawal amount must be greater than 0");
         require(_amount <= balances[msg.sender], "Insufficient balance");
 
@@ -25,6 +37,7 @@ contract SimpleWallet {
     }
 
     function transfer(address _recipient, uint256 _amount) external {
+        require(hasAccount[msg.sender], "Account does not exist");
         require(_amount > 0, "Transfer amount must be greater than 0");
         require(_amount <= balances[msg.sender], "Insufficient balance");
         require(_recipient != address(0), "Invalid recipient address");
